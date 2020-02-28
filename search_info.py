52165cf726d9e788498f3b54b91010c6774e151f
=KRAJACOB <=krakouassijacob@yahoo.fr>
2020-02-28 19:16:16 +0100

    read on file

--- search_info.py
+++ search_info.py
@@ -1,5 +1,20 @@
 import glob
+import json
 dossier = "D:\PERSO\python\cours_tp\dossier_exemple\**"
 files = glob.glob(dossier, recursive=True)
-for folder in files:
-    print(folder)
+for f in files:
+    if f.endswith('.json'):
+        with open(f, 'r') as file:
+            contenu = json.load(file)
+            if 'Credit Mutuel' in contenu:
+                credit_mutuel = contenu["Credit Mutuel"]["Numero de compte"]
+    elif f.endswith('.txt'):
+        #print(f)
+        with open(f, 'r',encoding="utf-8") as fichier:
+            contenu = fichier.read()
+            #print(contenu)
+            if "Numéro de sécurité sociale" in contenu:
+                securit_social = contenu.split(':')[-1]
+                #print("ok")
+print(f'le numéro de crédit mutuel est {credit_mutuel}')
+print(f'le numéro de sécurité sociale est {securit_social}')
